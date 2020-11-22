97e0ebe3f8b10071dd08557aeea2522b8dd84155
feizhijun <feizhijun@bytedance.com>
2020-11-22 17:39:02 +0800

    feat: box element draw

--- src/index.js
+++ src/index.js
@@ -51,69 +51,103 @@ let eyeX = 0,
  */
 const drawViewTriangle = (gl, program) => {
   const verteicesColors = new Float32Array([
-    // right
-    0.0, //blue
+    1.0, // 1
     1.0,
-    -2.0,
-    0.4,
-    0.4,
     1.0,
-    -0.5,
+    1.0,
+    1.0,
+    1.0,
+    -1.0, // 2
+    1.0,
+    1.0,
+    1.0,
+    0.0,
+    1.0,
+    -1.0, // 3
     -1.0,
-    -2.0,
-    0.4,
-    0.4,
     1.0,
-    0.5,
+    1.0,
+    0.0,
+    0.0,
+    1.0, // 4
     -1.0,
-    -2.0,
-    1.0,
-    0.4,
-    0.4,
-    0.0, // yellow
-    1.0,
-    -2.0,
     1.0,
     1.0,
-    0.4,
-    -0.5,
+    1.0,
+    0.0,
+    1.0, // 5
     -1.0,
-    -2.0,
-    1.0,
-    1.0,
-    0.4,
-    0.5,
     -1.0,
-    -2.0,
+    0.0,
     1.0,
-    0.4,
-    0.4,
-    0, // green
+    0.0,
+    1.0, // 6
     1.0,
-    -4.0,
-    0.4,
-    1.0,
-    0.4,
-    -0.5,
     -1.0,
-    -4.0,
-    0.4,
+    0.0,
     1.0,
-    0.4,
-    0.5,
+    1.0,
+    -1.0, // 7
+    1.0,
     -1.0,
-    -4.0,
+    0.0,
+    0.0,
     1.0,
-    0.4,
-    0.4,
+    -1.0, // 8
+    -1.0,
+    -1.0,
+    0.0,
+    0.0,
+    0.0,
+  ]);
+  const indices = new Uint8Array([
+    0, // 1
+    1,
+    2,
+    0,
+    2,
+    3,
+    0, // 2
+    3,
+    4,
+    0,
+    4,
+    5,
+    0, // 3
+    5,
+    6,
+    0,
+    6,
+    1,
+    1, // 4
+    6,
+    7,
+    1,
+    7,
+    2,
+    7, // 5
+    4,
+    3,
+    7,
+    3,
+    2,
+    4, // 6
+    7,
+    6,
+    4,
+    6,
+    5,
   ]);
   const vertexColorBuffer = gl.createBuffer();
-  if (!vertexColorBuffer) {
+  const indexBuffer = gl.createBuffer();
+  if (!vertexColorBuffer || !indexBuffer) {
     console.log("Failed to create buffer");
     return -1;
   }
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, verteicesColors, gl.STATIC_DRAW);
+  gl.enable(gl.DEPTH_TEST);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
   const a_Position = gl.getAttribLocation(program, "a_Position");
   const a_Color = gl.getAttribLocation(program, "a_Color");
   const u_MvpMatrix = gl.getUniformLocation(program, "u_MvpMatrix");
@@ -121,33 +155,27 @@ const drawViewTriangle = (gl, program) => {
     console.log("invalid storage");
     return;
   }
-  const modelMatrix = mat4.create();
-  const viewMatrix = mat4.create();
-  const projMatrix = mat4.create();
-  const mvpMatrix = mat4.create();
-  mat4.fromTranslation(modelMatrix, vec3.fromValues(0.75, 0, 0));
-  mat4.lookAt(
-    viewMatrix,
-    vec3.fromValues(eyeX, 0, 5),
-    vec3.fromValues(0, 0, -100),
-    vec3.fromValues(0, 1, 0)
-  );
-  mat4.perspective(projMatrix, 30, canvasRatio, 1, 100);
-  mat4.multiply(mvpMatrix, viewMatrix, modelMatrix);
-  mat4.multiply(mvpMatrix, projMatrix, mvpMatrix);
-  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix);
   const F_SIZE = verteicesColors.BYTES_PER_ELEMENT;
   gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, F_SIZE * 6, 0);
   gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, F_SIZE * 6, F_SIZE * 3);
   gl.enableVertexAttribArray(a_Position);
   gl.enableVertexAttribArray(a_Color);
-  gl.drawArrays(gl.TRIANGLES, 0, 9);
-  mat4.fromTranslation(modelMatrix, vec3.fromValues(-0.75, 0, 0));
-  mat4.multiply(mvpMatrix, viewMatrix, modelMatrix);
-  mat4.multiply(mvpMatrix, projMatrix, mvpMatrix);
+  const viewMatrix = mat4.create();
+  const projMatrix = mat4.create();
+  const mvpMatrix = mat4.create();
+  mat4.lookAt(
+    viewMatrix,
+    vec3.fromValues(3, 3, 7),
+    vec3.fromValues(0, 0, 0),
+    vec3.fromValues(0, -1, 0)
+  );
+  mat4.perspective(projMatrix, 30, 1, 1, 100);
+  mat4.multiply(mvpMatrix, projMatrix, viewMatrix);
   gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix);
-  gl.drawArrays(gl.TRIANGLES, 0, 9);
-  window.requestAnimationFrame(() => drawViewTriangle(gl, program));
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+  gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_BYTE, 0);
+  // window.requestAnimationFrame(() => drawViewTriangle(gl, program));
 };
 
 async function main() {
@@ -155,19 +183,9 @@ async function main() {
   const canvas = document.querySelector("#root");
   canvasRatio = canvas.width / canvas.height;
   const gl = canvas.getContext("webgl");
-  gl.enable(gl.DEPTH_TEST | gl.POLYGON_OFFSET_FILL);
-  gl.clear(gl.DEPTH_BUFFER_BIT);
-  gl.polygonOffset(1.0, 1.0);
   const program = await initShader(gl);
   gl.useProgram(program);
   drawViewTriangle(gl, program);
-  document.onkeydown = (ev) => {
-    if (ev.key === "ArrowRight") {
-      eyeX += 0.01;
-    } else if (ev.key === "ArrowLeft") {
-      eyeX -= 0.01;
-    }
-  };
 }
 
 main();
